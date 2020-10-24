96fd9ed1300ffc31baf14eb600b1196fcd3e47dc
ashu3902 <moreashutosh1997@gmail.com>
2020-10-24 13:38:12 +0530

    Added: Dictionary storage

--- arith.sh
+++ arith.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+declare -A Dic
 echo "This is Sorting arithmetic computation problem"
 echo "Enter 3 values"
 read a b c
@@ -10,3 +11,7 @@ res3=$(( $c + $a / $b ))
 echo "$res3"
 res4=$(( $a % $b + $c ))
 echo "$res4"
+Dic[1]=$res1
+Dic[2]=$res2
+Dic[3]=$res3
+Dic[4]=$res4
